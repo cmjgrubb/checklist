2bf651d464ebe7c4630e03c9897e9733d0c9b0ee
CMJ Grubb <cmj.grubb@icloud.com>
2024-04-18 16:59:33 -0400

    Started project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<h1>Review Checklist</h1>
+<p>
+	This checklist is to be completed by the design professional and submitted with the site plan.
+</p>
+
+<style>
+	:global(body) {
+		font-family: Arial, sans-serif;
+		font-size: 16px;
+		margin: 1.5rem;
+		padding: 0rem;
+		line-height: 2;
+	}
+
+	h1 {
+		font-size: 32px;
+	}
+
+	h2 {
+		color: #014b92;
+		font-size: 26px;
+		margin-top: 2rem;
+		margin-bottom: 0.5rem;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+	<div class="logo">
+		<img src="/images/logo.png" alt="Logo" style="width: 250px" />
+	</div>
+	<div class="break_line"></div>
+</header>
+
+<style>
+	.logo {
+		margin-top: 3rem;
+		margin-bottom: 2rem;
+		text-align: center;
+	}
+
+	.break_line {
+		width: 90%;
+		border-bottom: 5px solid #c0da2f;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 2rem;
+	}
+</style>
